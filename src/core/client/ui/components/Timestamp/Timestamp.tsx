e42c1022e3a83ee8fb082290370e9bfb6a0bb7e4
okbel <curciobelen@gmail.com>
2018-07-05 11:53:35 -0300

    Timestamp support for translations

--- src/core/client/ui/components/Timestamp/Timestamp.tsx
+++ src/core/client/ui/components/Timestamp/Timestamp.tsx
@@ -3,18 +3,29 @@ import TimeAgo from "react-timeago";
 
 import { withStyles } from "talk-ui/hocs";
 import { PropTypesOf } from "talk-ui/types";
+import { formatter } from "../../../framework/lib/i18n";
 
 import * as styles from "./Timestamp.css";
 
 interface InnerProps {
   date: string;
+  live?: boolean;
   classes: typeof styles;
+  formatter?: Formatter;
 }
 
 class Timestamp extends React.Component<InnerProps> {
   public render() {
-    const { date, classes } = this.props;
-    return <TimeAgo date={date} className={classes.root} />;
+    const { date, classes, live } = this.props;
+    // console.log(formatter(5, "minute", "ago"));
+    return (
+      <TimeAgo
+        date={date}
+        className={classes.root}
+        live={live}
+        formatter={formatter}
+      />
+    );
   }
 }
 
--- src/types/react-timeago.d.ts
+++ src/types/react-timeago.d.ts
@@ -3,9 +3,51 @@ declare module "react-timeago" {
 
   interface Props {
     date: string;
+    live?: boolean;
     className: string;
+    formatter?: Formatter;
   }
 
   function TimeAgo(): React.Component<Props>;
   export default TimeAgo;
+}
+
+interface LocaleDefinition {
+  prefixAgo?: string | null;
+  prefixFromNow?: string | null;
+  suffixAgo?: string | null;
+  suffixFromNow?: string | null;
+  second?: string | null;
+  seconds?: string | null;
+  minute?: string | null;
+  minutes?: string | null;
+  hour?: string | null;
+  hours?: string | null;
+  day?: string | null;
+  days?: string | null;
+  week?: string | null;
+  weeks?: string | null;
+  month?: string | null;
+  months?: string | null;
+  year?: string | null;
+  years?: string | null;
+  wordSeparator?: string;
+  numbers?: number[];
+}
+
+type Formatter = (
+  value: number,
+  unit: string,
+  suffix: string,
+  epochMiliseconds?: string
+) => string;
+
+declare module "react-timeago/lib/formatters/buildFormatter" {
+  function buildFormatter(localeInput: LocaleDefinition): Formatter;
+  export default buildFormatter;
+}
+
+declare module "react-timeago/lib/language-strings/*" {
+  const localeStrings: LocaleDefinition;
+  export default localeStrings;
 }
